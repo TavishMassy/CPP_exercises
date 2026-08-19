36ddb90a74a5c0193f10c65df542dd8a21cff9ec
TavishMassy <shaunmassy@gmail.com>
2026-08-19 23:39:31 +0530

    fix: simplified the operation and removed _primes

--- 32-prime-factors/prime_factors.cpp
+++ 32-prime-factors/prime_factors.cpp
@@ -6,44 +6,20 @@ namespace prime_factors {
 
 std::vector<long long> of(long long num) {
     std::vector<long long> factors{};
-    long long srqt_mid{static_cast<long long>(ceil(sqrt(num * 1.0)))};
-    if (srqt_mid > prime_nums.back()) {
-        _primes(srqt_mid);
-    }
-    int i{0};
+    long long i{2};
     while (num > 1) {
-        if (i >= static_cast<int>(prime_nums.size())) {
+        if (i * i > num) {
             factors.emplace_back(num);
             break;
         }
-        long long prime_num = prime_nums.at(i);
-        if (num % prime_num == 0) {
-            factors.emplace_back(prime_num);
-            num /= prime_num;
+        if (num % i == 0) {
+            factors.emplace_back(i);
+            num /= i;
         } else {
             ++i;
         }
     }
     return factors;
-}
-
-void _primes(long long& till) {
-    bool found_prime = true;
-    int i{1};
-    while (prime_nums.back() < till) {
-        for (long long& prime_num : prime_nums) {
-            if ((prime_nums.back() + i) % prime_num == 0) {
-                ++i;
-                found_prime = false;
-                break;
-            }
-        }
-        if (found_prime) {
-            prime_nums.emplace_back(prime_nums.back() + i);
-            i = 1;
-        }
-        found_prime = true;
-    }
 }
 
 }  // namespace prime_factors
